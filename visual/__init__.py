183dc8dec6633f44179bb302f84a90d7ae57cdee
Krzysztof Cybulski <k.cybulski.dev@gmail.com>
2017-12-28 02:27:30 +0100

    Basic image shape validation
    Images must have an even number of columns and rows in order to be
    turnable into a sequence, which is now handled by file_to_array.
    Some algorithms might be unable to sequence specific shapes of images,
    for instance non-square ones, and in such cases they should raise
    InvalidShapeError.

--- visual/__init__.py
+++ visual/__init__.py
@@ -12,12 +12,30 @@ import imageio
 import numpy as np
 from matplotlib import pyplot as plt
 
+class InvalidShapeError(Exception):
+    """
+    Raised when an algorithm receives an image with a shape 
+    it cannot turn into a sequence.
+    """
+
 def file_to_array(file_name):
-    return  np.array(imageio.imread(file_name))
+    """
+    Takes an image and returns a numpy array of its colour values.
+    If the image length or width aren't even it is cut by one 
+    row or column so that it is.
+    """
+    array = np.array(imageio.imread(file_name))
+    if array.shape[0]%2 != 0:
+        array = np.delete(array, array.shape[0], 0)
+    if array.shape[1]%2 != 0:
+        array = np.delete(array, array.shape[1], 1)
+    return array
 
 def array_to_file(array, file_name):
+    """Takes a numpy array and saves it into an image file."""
     imageio.imwrite(file_name, array) 
 
 def show(array):
+    """Shows an array as a picture on a matplotlib plot."""
     plt.imshow(array)
     plt.show()
